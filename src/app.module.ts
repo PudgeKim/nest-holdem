fa9dfba053eeb7058111a8f42778814a7c826271
PudgeKim <zxaq15@gmail.com>
2021-09-02 16:13:20 +0900

    add redis add redis in docker-compose add redisadapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import { GameService } from './game/game.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GameService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { RedisIoAdapter } from './game/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
+  app.useWebSocketAdapter(new RedisIoAdapter(app));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Texas-Holdem API')
